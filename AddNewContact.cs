67b00b305a36be0578ced0198362860884fa288f
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-19 17:27:53 +0530

    Added Program to Add new contact

---
+++ AddNewContact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddNewContact
+    {
+
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string Pincode;
+        public string State;
+        public string PhoneNumber;
+        public string Email;
+
+        public  AddNewContact(string fname, string lname, string address, string city, string pincode, string state, string phone, string email)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.Address = address;
+            this.City = city;
+            this.Pincode = pincode;
+            this.State = state;
+            this.PhoneNumber = phone;
+            this.Email = email;
+
+            Console.WriteLine("First Name : " + FirstName);
+            Console.WriteLine("Last Name : " + LastName);
+            Console.WriteLine("Address : " + Address);
+            Console.WriteLine("City Name : " + City);
+            Console.WriteLine("Pincode : " + Pincode);
+            Console.WriteLine("State : " + State);
+            Console.WriteLine("Phone Number : " + PhoneNumber);
+            Console.WriteLine("Email : " + Email);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AddressBookSystem
+﻿using System.Net;
+
+namespace AddressBookSystem
 {
     public class Program
     {
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+            Console.WriteLine("Please Enter Contact Details : firstname, lastname, email, address, city, state, zipCode, phone");
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            string Address = Console.ReadLine();
+            string City = Console.ReadLine();
+            string zipCode = Console.ReadLine();
+            string State = Console.ReadLine();
+            string PhoneNumber = Console.ReadLine();
+            string Email = Console.ReadLine();
+
+            AddNewContact contact1 = new AddNewContact(FirstName,LastName,Address,City,zipCode,State,PhoneNumber,Email);
         }
     }
 }
