cb6aaa9197d6a152f0314844d49d04a275b1a873
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-19 23:48:10 +0530

    Refactor Program to Add New Contact

--- Program.cs
+++ Program.cs
@@ -8,17 +8,11 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Welcome to the Address Book System");
 
-            Console.WriteLine("Please Enter Contact Details : firstname, lastname, email, address, city, state, zipCode, phone");
-            string FirstName = Console.ReadLine();
-            string LastName = Console.ReadLine();
-            string Address = Console.ReadLine();
-            string City = Console.ReadLine();
-            string zipCode = Console.ReadLine();
-            string State = Console.ReadLine();
-            string PhoneNumber = Console.ReadLine();
-            string Email = Console.ReadLine();
+           
 
-            AddNewContact contact1 = new AddNewContact(FirstName,LastName,Address,City,zipCode,State,PhoneNumber,Email);
+            AddNewContact contact1 = new AddNewContact();
+            contact1.AddDetails();
+            contact1.ListingPeople();
         }
     }
 }
