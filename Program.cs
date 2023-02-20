d2c2167ea1d31e3de00ebdeca2ed21452b781a97
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-20 12:21:32 +0530

    Added Program to Delete Contact from the list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+            DeleteContact  person = new DeleteContact();
+
+            //----Adding Contact---//
+            person.addContact();
+
+            //------Deleting Contact----//
+            person.deleteContact();
+            person.ListingPeople();
+
+            
         }
     }
 }
