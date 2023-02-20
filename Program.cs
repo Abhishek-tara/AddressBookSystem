805d730432148197b7f5c90d5ebbfa58b103cb49
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-20 12:56:21 +0530

    Added Program to Add Multiple Contact

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+            AddMultipleContact person = new AddMultipleContact();
+
+            person.addContact();
+            person.ListingPeople();
         }
     }
 }
