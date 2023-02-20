4a6bc904cc55f04bef2b9b8df178569faaccf1e4
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-20 11:01:48 +0530

    Added Program to Edit the Contact details

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+            EditContact person = new EditContact();
+
+            //-----Add contact----//
+            person.AddDetails();
+            //person.ListingPeople();
+
+
+            //-----Modify Contact-----//
+            person.editContact();
+            person.ListingPeople();
         }
     }
 }
