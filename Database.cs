958858b92d07c79d4f5f345484a406378f48154d
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-23 11:25:53 +0530

    Added: Update Database As Edit Contact In Address Book

--- Database.cs
+++ Database.cs
@@ -48,5 +48,42 @@ namespace AddressBookSystem
                 connection.Close();
             }
         }
+
+        // ability to update contact in database
+        private void EditContactDatabase(string name, Contact contact)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB Address_Book1;Initial Catalog=AddressBookDatabase;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                using (connection)
+                {
+                    string spName = "dbo.SpUpdateContact";
+                    SqlCommand command = new SqlCommand(spName, connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@firstName", contact.FirstName);
+                    command.Parameters.AddWithValue("@lastName", contact.LastName);
+                    command.Parameters.AddWithValue("@address", contact.Address);
+                    command.Parameters.AddWithValue("@city", contact.City);
+                    command.Parameters.AddWithValue("@state", contact.State);
+                    command.Parameters.AddWithValue("@zip", contact.Zip);
+                    command.Parameters.AddWithValue("@phoneNumber", contact.PhoneNumber);
+                    command.Parameters.AddWithValue("@email", contact.Email);
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    string outputMessage = result == 1 ? "Contact Updaeted SuccessFully" : "Contact Update failed";
+                    Console.WriteLine(outputMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
